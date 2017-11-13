8cbafcd92788253c905032a5f6908f8b760c7eb4
Wanderson Reis <wsreis@gmail.com>
2017-11-13 17:02:15 -0200

    Add new translation files.

---
+++ app/js/translations.js
@@ -0,0 +1,31 @@
+const i18next = require('i18next');
+const i18nextXHRBackend = require('i18next-xhr-backend');
+const jqueryI18next = require('jquery-i18next');
+
+i18next
+.use(i18nextXHRBackend)
+.init({
+    fallbackLng: 'en-US',
+    debug: false,
+    ns: ['deimos-issuer'],
+    defaultNS: 'deimos-issuer',
+    backend: {
+      loadPath: 'locales/{{lng}}/{{ns}}.json'
+    }
+}, function(err, t) {
+    jqueryI18next.init(i18next, $);
+    $('body').localize();
+    $('#language').change(function() {
+      i18next.changeLanguage($('#language').val(), function() {
+        ipcRenderer.send('language-change', $('#language').val());
+         $('body').localize();
+
+      });
+    });
+});
+
+function loadTranslations() {
+    const language = remote.getGlobal('settings').language;
+    $('#language').val(language);
+    $('#language').trigger('change');
+}
